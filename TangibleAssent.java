b1f02a1ee2e0158a9f837b8d6d9393b1768c02e1
takai-free <wbybc19223@yahoo.co.jp>
2025-07-16 17:33:23 +0900

    Create TangibleAssent.java

---
+++ TangibleAssent.java
@@ -0,0 +1,17 @@
+public abstract class TangibleAssent{
+    private String name;
+    private int price;
+    private String color;
+
+ public TangibleAssent(String name, int price, String color){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+
+}
+
+public String getname(){returm this.name;}
+public int getPrice(){returm this.price;}
+public String getColor(){returm this.color;}
+
+}
